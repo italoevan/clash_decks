3e62e7f569ef4d444d03fc7f19ebcc2f990e6852
Italo Gabriel <igbehh@gmail.com>
2022-04-08 18:54:47 -0300

    att

---
+++ micro_app_home/lib/micro_app_home_module.dart
@@ -0,0 +1,9 @@
+import 'package:commons_dependencies/commons_dependencies.dart';
+
+class MicroAppHomeModule extends Module {
+
+  @override
+  // TODO: implement routes
+  List<ModularRoute> get routes => super.routes;
+
+}
